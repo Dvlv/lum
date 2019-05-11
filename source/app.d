ada3e97935ff597cfab387183efe1aee9ea3058c
Dvlv <dvd292@googlemail.com>
2019-05-11 17:08:00 +0100

    password box, change username feature

--- source/app.d
+++ source/app.d
@@ -3,6 +3,7 @@ import std.process;
 import std.string;
 
 import gtk.MainWindow;
+import gtk.Window;
 import gtk.Main;
 import gtk.Entry;
 import gtk.Button, gtk.Widget, gtk.Box, gdk.Event;
@@ -10,35 +11,37 @@ import gtk.Label;
 import gtk.Grid;
 import gtk.ListBox;
 import gtk.ListBoxRow;
+import gtk.MessageDialog, gtk.Dialog;
+import gtk.VBox;
 
 
 
 string getOutput(string[] cmd) {
-	auto pipes = pipeProcess(cmd, Redirect.stdout | Redirect.stderr);
-	scope(exit) wait(pipes.pid);
+    auto pipes = pipeProcess(cmd, Redirect.stdout | Redirect.stderr);
+    scope(exit) wait(pipes.pid);
 
-	string output = strip(pipes.stdout.readln());
-	
-	return output;	
+    string output = strip(pipes.stdout.readln());
+    
+    return output;	
 }
 
 string getUserRealName(string username) {
-	string passOut = getOutput(["getent", "passwd", username]);
-	if (!passOut.length) {
-		return "";
-	}
-	auto cutPipe = pipeProcess(["cut", "-f", "5", "-d", ":"], Redirect.all);
-	cutPipe.stdin.writeln(passOut);
-	cutPipe.stdin.close();
+    string passOut = getOutput(["getent", "passwd", username]);
+    if (!passOut.length) {
+        return "";
+    }
+    auto cutPipe = pipeProcess(["cut", "-f", "5", "-d", ":"], Redirect.all);
+    cutPipe.stdin.writeln(passOut);
+    cutPipe.stdin.close();
 
-	scope(exit) wait(cutPipe.pid);
-	
-	auto realName = strip(cutPipe.stdout.readln());
-	if (realName.length > 0) {
-		return realName;
-	} else {
-		return "";
-	}
+    scope(exit) wait(cutPipe.pid);
+    
+    auto realName = strip(cutPipe.stdout.readln());
+    if (realName.length > 0) {
+        return realName;
+    } else {
+        return "";
+    }
 }
 
 string[] getAllAvailableUsers()
@@ -66,31 +69,45 @@ string[] getAllAvailableUsers()
 
 class UserManagerMain
 {
-	MainWindow win = null;
-	int defaultPadding = 20;
-	Grid mainGrid = null;
-	
-	string launchedUser = "";
-	
-	ListBox usersList = null;
-	InputsGrid inputsGrid = null;
-	Box buttonBox = null;
+    MainWindow win = null;
+    int defaultPadding = 20;
+    Grid mainGrid = null;
+    
+    string launchedUser = "";
+    
+    ListBox usersList = null;
+    InputsGrid inputsGrid = null;
+    Box buttonBox = null;
+    
+    string currentlySelectedUser = null;
+    bool isRoot = false;
 
-	this(string[] argv) {
-		Main.init(argv);
-		this.win = new MainWindow("User Manager");
-		this.win.setDefaultSize(600, 300);
+    this(string[] argv, bool isRoot) {
+        Main.init(argv);
+        this.win = new MainWindow("User Manager");
+        this.win.setDefaultSize(750, 300);
 
-		this.mainGrid = new Grid();
-		this.mainGrid.setBorderWidth(20);
-		this.mainGrid.setRowSpacing(10);
-	}
-	
-	ListBox getExistingUsersListBox() {
+        this.mainGrid = new Grid();
+        this.mainGrid.setBorderWidth(20);
+        this.mainGrid.setRowSpacing(10);
+        
+        this.isRoot = isRoot;
+    }
+    
+    ListBox getExistingUsersListBox() {
         ListBox usersList = new ListBox();
         usersList.setHexpand(true);
         
-        string[] users = getAllAvailableUsers();
+        string[] users = [];
+        
+        if (this.isRoot) {
+            foreach(u; getAllAvailableUsers()) {
+                users ~= u;
+            }
+        } else {
+            users ~= this.launchedUser;
+        }
+        
         if (users.length == 0) {
             return usersList;
         }
@@ -98,105 +115,213 @@ class UserManagerMain
         foreach(user; users) {
             ListBoxRow row = new ListBoxRow();
             Label userLabel = new Label(user);
-            row.add(userLabel);
             
+            row.add(userLabel);
             usersList.add(row);
+            
+            if (user == this.launchedUser) {
+                usersList.selectRow(row);
+            }   
         }
         
         usersList.addOnRowSelected(&this.showUserInfo);
         
         return usersList;
-        
-       
-	}
-	
-	void showUserInfo(ListBoxRow row, ListBox lb) {
+    }
+    
+    void showUserInfo(ListBoxRow row, ListBox lb) {
         Widget label = row.getChild();
         Label userLabel = cast(Label) label;
         string username = userLabel.getText();
         
         this.inputsGrid.fillInUserDetails(username);
-	}
-	
-	void addCreateInputs() {
+        this.currentlySelectedUser = username;
+    }
+    
+    void addCreateInputs() {
         this.inputsGrid = new InputsGrid();
         this.inputsGrid.fillInUserDetails(this.launchedUser);
         
-		this.mainGrid.attach(this.inputsGrid, 1, 0, 1, 1);
-	}
+        this.mainGrid.attach(this.inputsGrid, 1, 0, 1, 1);
+    }
 
-	void addButtons() {
-		auto changePassButton = new Button("Change Password");
-		auto saveButton = new Button("Save");
-		auto cancelButton = new Button("Cancel");
+    void addButtons() {
+        auto changePassButton = new Button("Change Password");
+        auto saveButton = new Button("Save");
+        auto cancelButton = new Button("Cancel");
 
-		Box buttonBox = new Box(Orientation.HORIZONTAL, 30);
-		buttonBox.setBorderWidth(20);
-		buttonBox.setHomogeneous(true);
-		
-		buttonBox.add(saveButton);
-		buttonBox.add(changePassButton);
-		buttonBox.add(cancelButton);
+        saveButton.addOnClicked(&saveChanges);
+        changePassButton.addOnClicked(&showChangePasswordDialog);
+    
+        Box buttonBox = new Box(Orientation.HORIZONTAL, 30);
+        buttonBox.setBorderWidth(20);
+        buttonBox.setHomogeneous(true);
+        
+        buttonBox.add(saveButton);
+        buttonBox.add(changePassButton);
+        buttonBox.add(cancelButton);
 
-		this.mainGrid.attach(buttonBox, 1, 1, 1, 1);
-	}
+        this.mainGrid.attach(buttonBox, 1, 1, 1, 1);
+    }
+        
+    void saveChanges(Button b) {
+        string realName = this.inputsGrid.getRealNameText();
+        if (realName.length && realName != getUserRealName(currentlySelectedUser)) {
+            auto chfnPipe = pipeProcess(["chfn", "-f", format("'%s'", realName), this.currentlySelectedUser], Redirect.stderr | Redirect.stdout);
+            if (wait(chfnPipe.pid) != 1) {
+                showCouldNotUpdateUserError();
+            }
+            
+            string chfnOut = strip(chfnPipe.stdout.readln());
+            string chfnErr = strip(chfnPipe.stderr.readln());
+            
+            if (chfnErr.length && (indexOf(chfnErr, "login.defs forbids") > -1)) {
+                showCouldNotUpdateUserError();
+            }
+        }
+    }
+    
+    void showCouldNotUpdateUserError() {
+        MessageDialog errorMsg = new MessageDialog(this.win, DialogFlags.MODAL, MessageType.ERROR, ButtonsType.CLOSE, 
+                                                    "Could not update user, please contact your administrator.");
+        errorMsg.addOnResponse(
+            (int responseId, Dialog dlg) => dlg.destroy()
+        );
+        
+        errorMsg.show();
+    }
+    
+    void showChangePasswordDialog(Button b) {
+        auto cpd = new ChangePasswordDialog(this.win);
+        cpd.setTransientFor(this.win);
+        cpd.run();
+    }
 
-	void run() {
+    void run() {
         this.usersList = getExistingUsersListBox();
         this.mainGrid.attach(this.usersList, 0, 0, 1, 3);
         
-		this.win.add(mainGrid);
-		win.showAll();
-		Main.run();
-	}
+        this.win.add(mainGrid);
+        win.showAll();
+        Main.run();
+    }
 }
 
 class InputsGrid : Grid
 {
     Entry usernameEntry = null;
-	Entry realNameEntry = null;
-	Grid inputsGrid = null;
-     
+    Entry realNameEntry = null;
+    Grid inputsGrid = null;
+    
     this() {
         this.usernameEntry = new Entry();
-		this.usernameEntry.setHexpand(true);  // only need to do this once I guess.
-     
-		this.realNameEntry = new Entry();
-		this.layoutInputs();
+        this.usernameEntry.setHexpand(true);  // only need to do this once I guess.
+        this.usernameEntry.setProperty("editable", false);
+        this.usernameEntry.setSensitive(false);
+    
+        this.realNameEntry = new Entry();
+        this.layoutInputs();
     }
     
     void layoutInputs() {
-		this.setColumnSpacing(10);
-		this.setRowSpacing(10);
-		this.setBorderWidth(10);
+        this.setColumnSpacing(10);
+        this.setRowSpacing(10);
+        this.setBorderWidth(10);
 
-		Label usernameLabel = new Label("Username:");
-		Label realNameLabel = new Label("Real Name:");
-		
-		this.attach(usernameLabel, 1, 0, 1, 1);
-		this.attach(this.usernameEntry, 2, 0, 1, 1);
+        Label usernameLabel = new Label("Username:");
+        Label realNameLabel = new Label("Real Name:");
+        
+        this.attach(usernameLabel, 1, 0, 1, 1);
+        this.attach(this.usernameEntry, 2, 0, 1, 1);
 
-		this.attach(realNameLabel, 1, 1, 1, 1);
-		this.attach(this.realNameEntry, 2, 1, 1, 1);
+        this.attach(realNameLabel, 1, 1, 1, 1);
+        this.attach(this.realNameEntry, 2, 1, 1, 1);
     }
     
     void fillInUserDetails(string username) {
-		this.usernameEntry.setText(username);
-		
-		string realName = getUserRealName(username);
-		if (realName) {
-			this.realNameEntry.setText(realName);
-		}
-	}
+        this.usernameEntry.setText(username);
+        
+        string realName = getUserRealName(username);
+        if (realName) {
+            this.realNameEntry.setText(realName);
+        }
+    }
+    
+    string getRealNameText() {
+        return this.realNameEntry.getText();
+    }
+}
+
+
+class ChangePasswordDialog : Dialog 
+{
+
+    Entry oldPasswordEntry = null;
+    Entry newPasswordEntry = null;
+    Entry confirmNewPasswordEntry = null;
+    
+    this(Window parent) {
+        super("Change Your Password", parent);
+        this.oldPasswordEntry = new Entry();
+        this.newPasswordEntry = new Entry();
+        this.confirmNewPasswordEntry = new Entry();
+        
+        this.oldPasswordEntry.setVisibility(false);
+        this.newPasswordEntry.setVisibility(false);
+        this.confirmNewPasswordEntry.setVisibility(false);
+        
+        auto oldPasswordLabel = new Label("Current Password: ");
+        auto newPasswordLabel = new Label("New Password: ");
+        auto confirmNewPasswordLabel = new Label("Confirm New Password: ");
+        
+        Box innerBox = new Box(Orientation.VERTICAL, 5);
+        innerBox.setBorderWidth(10);
+        
+        innerBox.add(oldPasswordLabel);
+        innerBox.add(this.oldPasswordEntry);
+        innerBox.add(newPasswordLabel);
+        innerBox.add(this.newPasswordEntry);
+        innerBox.add(confirmNewPasswordLabel);
+        innerBox.add(this.confirmNewPasswordEntry);
+        
+        Box contentBox = this.getContentArea();
+        contentBox.add(innerBox);
+        
+        this.showAll();
+        
+        this.addButton("Update", 1);
+        this.addButton("Cancel", 0);
+        
+        this.addOnResponse(&onButtonClicked);
+    }
+    
+    void onButtonClicked(int responseId, Dialog dlg) {
+        if(responseId == 0) {
+            this.destroy();
+        } else {
+            writeln("change pass");
+        }
+    }
+
+}
+
+bool isRootUser() {
+    auto idPipe = pipeProcess(["id", "-u"], Redirect.stderr | Redirect.stdout);
+    scope(exit) wait(idPipe.pid);
+    
+    auto op = strip(idPipe.stdout.readln());
+    return op == "0";
 }
 
 void main(string[] argv)
 {
     string me = getOutput(["whoami"]);
-	UserManagerMain umm = new UserManagerMain(argv);
-	umm.launchedUser = me;
-	umm.addCreateInputs();
-	umm.addButtons();
-	umm.run();
+    bool isRoot = isRootUser();
+    
+    UserManagerMain umm = new UserManagerMain(argv, isRoot);
+    umm.launchedUser = me;
+    umm.addCreateInputs();
+    umm.addButtons();
+    umm.run();
 }
 
